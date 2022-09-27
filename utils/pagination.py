801e2e9fcfe00dd64a8eb02f4ec9a0d6e1a05aa8
rhj-python <34661825+rhj-python@users.noreply.github.com>
2022-09-27 19:57:14 +0800

    Update pagination.py

--- utils/pagination.py
+++ utils/pagination.py
@@ -1,43 +1,5 @@
 # coding:utf-8
 
-# def pagination(current_page,total_num,single_page_num,show_page):
-#     total_page=total_num//single_page_num
-#     if total_num%single_page_num:
-#         total_page+=1
-#
-#     start=single_page_num*(current_page-1)
-#     end=start+single_page_num
-#
-#     pre_page=current_page-1
-#     if current_page<=1:
-#         pre_page=1
-#
-#     next_page=current_page+1
-#     if current_page>=total_page:
-#         next_page=total_page
-#
-#     tmp_page=current_page-1
-#     page_list=[]
-#
-#     while tmp_page>0:
-#         if tmp_page%show_page:
-#             page_list.append(tmp_page)
-#             tmp_page-=1
-#         else:
-#             break
-#
-#     tmp_page=current_page
-#
-#     while tmp_page<=total_page:
-#         page_list.append(tmp_page)
-#         if tmp_page%show_page:
-#             tmp_page+=1
-#         else:
-#             break
-#
-#     page_list.sort()
-#
-#     return (total_page,start,end,page_list,pre_page,next_page)
 
 def pagination(current_page,total_num,sing_page_num,show_page):
     current_page=int(current_page)
